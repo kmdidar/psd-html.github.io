6f7085e81f59577f309c47e1b58a6f3facb456ee
kmdidar <kmdidar@gmail.com>
2017-06-22 01:02:43 +0600

    class10

---
+++ class10/assets/js/main.js
@@ -0,0 +1,87 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $(".homepage-sliders").owlCarousel({
+            items: 1,
+            loop: true,
+            nav: true,
+            dots: true,
+            autoplay: true,
+            navText: ["<i class=\"zmdi zmdi-long-arrow-left\"></i>", "<i class=\"zmdi zmdi-long-arrow-right\"></i>"],
+            animateOut: "fadeOut",
+            animateIn: 'fadeIn'
+
+        });
+
+        $(".case-studies-carousel").owlCarousel({
+            items: 3,
+            margin: 30,
+            loop: true,
+            nav: false,
+            dots: true,
+            autoplay: true
+
+        });
+
+        $(".case-studies-carousel-3").owlCarousel({
+            items: 3,
+            margin: 0,
+            loop: true,
+            nav: false,
+            dots: true,
+            autoplay: true
+
+        });
+
+        $(".logo-carousel").owlCarousel({
+            items: 6,
+            margin: 30,
+            loop: true,
+            nav: false,
+            dots: true,
+            autoplay: true
+
+        });
+
+        $(".testimonial-content, .testimonial-item").owlCarousel({
+        	items: 1,
+        	loop: true,
+        	nav: false,
+        	dots: true,
+            autoplay: true
+
+        });
+
+            $(".testimonial-content").on("translate.owl.carousel", function(){ 
+            $(".single-testionial-item p").removeClass("animated fadeInUp").css("opacity", "0");
+            $(".testimonial-meta").removeClass("animated fadeInDown").css("opacity", "0");
+           
+        });
+        
+            $(".testimonial-content").on("translated.owl.carousel", function(){
+            $(".single-testionial-item p").addClass("animated fadeInUp").css("opacity", "0");
+            $(".testimonial-meta").addClass("animated fadeInDown").css("opacity", "0");
+          
+        });
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
